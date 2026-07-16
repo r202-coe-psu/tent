f4d462868bdf7f7a1f040916b8372be2b9ff4060
Keetasin <keetasin01596@gmail.com>
2026-07-16 19:20:07 +0700

    feat(kitchen): split meal plan/requisition/service into tabs

--- frontend/src/routes/(protected)/back-office/kitchen/+page.svelte
+++ frontend/src/routes/(protected)/back-office/kitchen/+page.svelte
@@ -7,6 +7,7 @@
 	} from '$lib/features/kitchen';
 	import { startOperationsLiveQuery } from '$lib/features/operations';
 	import { useQueryClient } from '@tanstack/svelte-query';
+	import * as Tabs from '$lib/components/ui/tabs';
 
 	const queryClient = useQueryClient();
 
@@ -27,9 +28,20 @@
 </svelte:head>
 
 <div class="flex-1 overflow-auto">
-	<MealPlanList />
-	<div class="space-y-4 px-4 pb-4">
-		<RequisitionHistory />
-		<MealServiceSummary />
-	</div>
+	<Tabs.Root value="plan" class="gap-0">
+		<Tabs.List class="mx-4 mt-4">
+			<Tabs.Trigger value="plan">แผนอาหาร</Tabs.Trigger>
+			<Tabs.Trigger value="requisition">ประวัติเบิก</Tabs.Trigger>
+			<Tabs.Trigger value="service">สรุปบริการ</Tabs.Trigger>
+		</Tabs.List>
+		<Tabs.Content value="plan">
+			<MealPlanList />
+		</Tabs.Content>
+		<Tabs.Content value="requisition" class="px-4 pb-4">
+			<RequisitionHistory />
+		</Tabs.Content>
+		<Tabs.Content value="service" class="px-4 pb-4">
+			<MealServiceSummary />
+		</Tabs.Content>
+	</Tabs.Root>
 </div>
