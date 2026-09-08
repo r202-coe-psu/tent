32865c13b5b165a0a8cad079bc11a0e52efb1ba7
NetLynx <jakee.indapanya@gmail.com>
2026-09-08 09:56:33 +0700

    fix: radio button circle icon

--- frontend/src/lib/components/ui/radio-group/radio-group-item.svelte
+++ frontend/src/lib/components/ui/radio-group/radio-group-item.svelte
@@ -16,14 +16,16 @@
 	{value}
 	data-slot="radio-group-item"
 	class={cn(
-		'aspect-square size-4 shrink-0 rounded-full border border-input text-primary shadow-xs transition-shadow outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:aria-invalid:ring-destructive/40',
+		'relative flex aspect-square size-4 shrink-0 items-center justify-center rounded-full border border-input text-primary shadow-xs transition-shadow outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:aria-invalid:ring-destructive/40',
 		className
 	)}
 	{...restProps}
 >
 	{#snippet children({ checked })}
 		{#if checked}
-			<CircleIcon class="size-2 fill-current" />
+			<CircleIcon
+				class="absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2 fill-current"
+			/>
 		{/if}
 	{/snippet}
 </RadioGroupPrimitive.Item>
